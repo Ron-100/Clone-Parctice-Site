f079b6640c4cd879630c3a1883608a96e7cbce25
Ron-100 <olamail909@gmail.com>
2025-03-05 00:48:31 +0530

    ExampleF push

---
+++ Clone site 08/index.js
@@ -0,0 +1,118 @@
+// window.onbeforeunload = function () {
+//     window.scrollTo(0, 0);
+// };
+
+// intro arrow
+document.addEventListener('DOMContentLoaded',function(){
+    let arrow = document.querySelector('.intro-arrow a');
+    arrow.addEventListener('mouseover',function(){
+        arrow.style.animationPlayState = "paused";
+    })
+    arrow.addEventListener('mouseout',function(){
+        arrow.style.animationPlayState = "running";
+    })
+})
+// intro arrow
+
+
+// ikib animation
+
+document.addEventListener('DOMContentLoaded',function(){
+    let ikib = document.querySelector('.name p');
+    let started = false;
+    
+    function ikibSlide(){
+        let elementPosition = ikib.getBoundingClientRect().top;
+        let windowHight = window.innerHeight;
+        
+        if(!started && elementPosition < windowHight){
+            started = true;
+            ikib.classList.add('show')
+        }
+    }
+    window.addEventListener('scroll',ikibSlide)
+})
+
+// ikib animation
+
+
+// min screen nav
+
+document.addEventListener('DOMContentLoaded',function(){
+    let menu = document.querySelector('.menu-btn');
+    let menuList = document.querySelector('.nav ul');
+    let links = document.querySelectorAll('.nav ul li a');
+    menu.addEventListener('click',function(){
+        menuList.classList.toggle('appear')
+        links.forEach(function(link){
+            link.classList.toggle('show')
+        })
+    })
+
+    const menubtn = document.querySelector('.menu-btn');
+    let nav1 = document.querySelector('.nav1');
+    let nav2 = document.querySelector('.nav2');
+    let nav3 = document.querySelector('.nav3');
+    menubtn.addEventListener('click', function(){
+        nav2.classList.toggle('nav2js')
+        nav1.classList.toggle('nav1js')
+        nav3.classList.toggle('nav3js')
+    })
+})
+
+// min screen nav
+
+
+// page marker
+
+document.addEventListener('DOMContentLoaded',function(){
+    let pages = document.querySelectorAll('.home, .intro2, .about, .services, .team, .history, .works, .contact');
+    let navLinks = document.querySelectorAll('.nav ul li');
+    
+    function activePage(){
+        let scrollPosition = window.scrollY + 200;
+        index = 0;
+        for (let i = 0; i < pages.length; i++) {
+            if (scrollPosition > pages[i].offsetTop) {
+                index = i;
+            }else{
+                break;
+            }
+        }
+        
+        navLinks.forEach(function(links){
+            links.classList.remove('active');
+        })
+        navLinks[index].classList.add('active');
+        
+    }
+
+    window.addEventListener('scroll', activePage);
+    activePage();
+})
+
+// page marker
+
+
+// home page animation
+
+document.addEventListener('DOMContentLoaded',function(){
+    let rSqu = document.querySelector('.r-squ')
+    let inrSqr = document.querySelector('.inr-sqr')
+    let inrSqrP = document.querySelector('.inr-sqr p')
+    let started = false;
+
+    window.addEventListener('scroll',function(){
+        let elementPosition = inrSqr.getBoundingClientRect().top;
+        let windowHeight = window.innerHeight;
+
+        if (!started && windowHeight > elementPosition) {
+            started = true
+            rSqu.classList.add('r-squani')
+            inrSqr.classList.add('inr-sqrani')
+            inrSqrP.classList.add('inr-sqraniT')
+        }
+    })
+})
+
+// home page animation
